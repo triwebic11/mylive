79342bd1dfb01b20d05a9e9da687820e7cb9924b
mdalihasanriyad <you@ahriyad.com@gmail.com>
2025-05-27 23:54:33 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+      <h1>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// ✅ Correct Vite config
+export default defineConfig({
+  plugins: [react()],
+});
