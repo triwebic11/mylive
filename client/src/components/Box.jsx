646667bda561f59ad214ba3941caa3454e17fa9c
Md Sujon Islam <iamsujonislam06@gmail.com>
2025-05-29 00:51:43 +0600

    home page 3 part desing

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Box = ({ icon, title, pragraph,bg }) => {
+const Box = ({ icon, title, pragraph, bg }) => {
   return (
-    <div className={`flex flex-col justify-center items-center gap-3 text-white ${bg} px-8 py-6 hover:scale-110 duration-500 cursor-pointer`}>
+    <div
+      className={`flex flex-col justify-center items-center gap-3 text-white ${bg} px-8 py-6 md:hover:scale-110 duration-500 cursor-pointer w-full mx-4`}
+    >
       <span className="text-4xl">{icon}</span>
       <h1 className="text-3xl">{title}</h1>
       <p>{pragraph}</p>
