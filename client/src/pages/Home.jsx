20822c35a3fe77a7bfa91b2a68ad9371113cb6c9
ayesha_siddika110 <ayeshamail418109@gmail.com>
2025-05-29 11:39:43 +0600

    add our products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HomeSlider from "../components/Slider";
+import OurProducts from "../components/OurProducts";
 
 
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div>
         <HomeSlider />
+        <OurProducts></OurProducts>
     </div>
   );
 };
