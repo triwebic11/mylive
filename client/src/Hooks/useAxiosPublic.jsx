aec9c5a2e9065c06b5ffd34a76b75012d88ccf9d
Md Sujon Islam <iamsujonislam06@gmail.com>
2025-07-04 12:18:21 +0600

    resolved merge conflict

--- client/src/Hooks/useAxiosPublic.jsx
+++ client/src/Hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://apidata.shslira.com/api",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://apidata.shslira.com/api",
 });
 
 const useAxiosPublic = () => {
