23b2347d3b9eb64c35d69402b144f184ace7a2cb
ayesha_siddika110 <ayeshamail418109@gmail.com>
2025-07-16 21:37:10 +0600

    add

--- server/models/User.js
+++ server/models/User.js
@@ -165,7 +165,7 @@ const entrySchema = new mongoose.Schema({
 
 
 const userSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+  // _id: mongoose.Schema.Types.ObjectId,
   name: String,
   email: String,
   phone: String,
