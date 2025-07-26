bac733ebf41ad19ab4769f22a716d5acd6be353e
Md Sujon Islam <iamsujonislam06@gmail.com>
2025-07-26 21:49:04 +0600

    nothing changes

--- client/src/components/OrderCreate.jsx
+++ client/src/components/OrderCreate.jsx
@@ -126,7 +126,6 @@ const OrderCreate = ({ title }) => {
     try {
       const res = await axiosSecure.post("/admin-orders", orderData);
 
-
       console.log(`orders ------- `, orderData);
       console.log("Order created:", res.data);
       if (res.data._id) {
@@ -296,7 +295,6 @@ const OrderCreate = ({ title }) => {
                           className="w-full border border-gray-300 px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                           placeholder="MRP"
                           required
-                          readOnly
                         />
                       </td>
 
